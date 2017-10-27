48e962b92a6f30621609017a897da34c8a1b6ff6
Animesh <animeshk55@gmail.com>
2017-10-27 11:39:51 +0530

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,5 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  //googleRedirectURI: 'https://bayou.herokuapp.com'
+  googleRedirectURI: 'https://bayou.herokuapp.com'
 };
