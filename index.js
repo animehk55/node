dc0a6306c88d7a7567d85ce3585f3af58d01f79e
Animesh <animeshk55@gmail.com>
2017-10-27 10:59:32 +0530

    finished auth flow

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cookieSession({
-    maxAge: 30* 24 * 60 * 60 * 1000,
+    maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
 );
@@ -24,5 +24,5 @@ app.use(passport.session());
 
 require('./routes/authRoutes') (app);
 
-const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
 app.listen(PORT);
